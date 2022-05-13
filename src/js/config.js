6d572481828c9e4f248818f001ab6a63a0b8ab24
Ichahal <aman.0002@icloud.com>
2022-05-13 22:30:34 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '36dc5def-8858-4973-9bf6-19dcc394a037';
+export const MODAL_CLOSE_SEC = 2.5;
